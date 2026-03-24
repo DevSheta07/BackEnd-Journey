1dc561d0b6136a16b2d1cd318e3dc579a2db2436
DevSheta07 <devsheta114@gmail.com>
2026-03-24 22:10:15 +0530

    add login and logout feature

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,27 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models.js";
+
+// Sometimes res not in used then write _ there
+export const verifyJWT = asyncHandler(async(req, _, next) => {
+    try {
+        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+    
+        if(!accessToken){
+            throw new ApiError(401, "Unauthorized: Access token is missing");
+        }
+    
+        const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+        if(!user){
+            throw new ApiError(401, "Invalid access token");
+        }
+    
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token");
+    }
+})
